384b74392f8f5012733fff219dd44020c7544630
Elijah Mazzella <Mizzo56@gmail.com>
2024-05-31 18:48:49 -0400

    Looping LAb complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const message = (["Bob", "Charles", "Joe"], "Thank You");
+
+function writeCards(message, string) {
+    let newArray=[]
+    for (let i = 0; i < message.length; i++) {
+    newArray.push(`Thank you, ${message[i]}, for the wonderful ${string} gift!`);
+    debugger;
+  }
+    return newArray ;
+}
+
+function countDown(number){
+    while (number >= 0) {
+console.log(number)
+--number 
+    }
+} 
